618747c9c4abc143f8856a40e3a918b66827b07a
KenHuang-Mac13 <bluetch@gmail.com>
2019-06-16 17:05:56 +0800

    Updates

---
+++ precache-manifest.d74f50ad1aea41a5be45c76faad5c9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfae2a458f335b95b7e5",
+    "url": "/static/js/main.dfae2a45.chunk.js"
+  },
+  {
+    "revision": "17afaf182502e8887a7d",
+    "url": "/static/js/1.17afaf18.chunk.js"
+  },
+  {
+    "revision": "dfae2a458f335b95b7e5",
+    "url": "/static/css/main.3dbc1215.chunk.css"
+  },
+  {
+    "revision": "17afaf182502e8887a7d",
+    "url": "/static/css/1.2d883382.chunk.css"
+  },
+  {
+    "revision": "9bf535d7cd37d78727036323fd4d28df",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e5618b28f08cefc3369947468c3aa2b.js"
+  "/precache-manifest.d74f50ad1aea41a5be45c76faad5c9a9.js"
 );
 
 workbox.clientsClaim();
