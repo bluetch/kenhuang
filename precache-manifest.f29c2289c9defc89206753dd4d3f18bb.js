2f8354b3913b05eba590cfdc36936d5f0b774514
Ken Huang <tzuchuan.huang@shopee.com>
2020-07-16 10:06:49 +0800

    Updates

---
+++ precache-manifest.f29c2289c9defc89206753dd4d3f18bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "115714293fa39cd8d373",
+    "url": "/static/css/main.619b0932.chunk.css"
+  },
+  {
+    "revision": "115714293fa39cd8d373",
+    "url": "/static/js/main.11571429.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "ffcecaaafc002e7c6893",
+    "url": "/static/css/2.991a3681.chunk.css"
+  },
+  {
+    "revision": "ffcecaaafc002e7c6893",
+    "url": "/static/js/2.ffcecaaa.chunk.js"
+  },
+  {
+    "revision": "04db441bd6d0d4923326cf2b1907bc82",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8a852a2bd3226f394ac2a38ce1a16ee.js"
+  "/precache-manifest.f29c2289c9defc89206753dd4d3f18bb.js"
 );
 
 workbox.clientsClaim();
