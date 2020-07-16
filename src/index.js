c9fef80ece0cfc3e30c6cfaed2039778975ad12b
Ken Huang <tzuchuan.huang@shopee.com>
2020-07-16 13:28:15 +0800

    update package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { render } from 'react-snapshot';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './modules/ScrollToTop'
