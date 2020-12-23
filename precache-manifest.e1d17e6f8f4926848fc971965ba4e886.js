7850629b5afa0da98b6081709c1246418bbebcfc
Ken Huang <tzuchuan.huang@shopee.com>
2020-12-23 12:14:22 +0800

    Updates

---
+++ precache-manifest.e1d17e6f8f4926848fc971965ba4e886.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d9950342e17abffdbcf",
+    "url": "/static/css/main.d0768b5b.chunk.css"
+  },
+  {
+    "revision": "4d9950342e17abffdbcf",
+    "url": "/static/js/main.4d995034.chunk.js"
+  },
+  {
+    "revision": "48c1f52f4997aca866be",
+    "url": "/static/js/runtime~main.48c1f52f.js"
+  },
+  {
+    "revision": "944e1dccbe4ef38cbfb2",
+    "url": "/static/css/2.1c70f99b.chunk.css"
+  },
+  {
+    "revision": "944e1dccbe4ef38cbfb2",
+    "url": "/static/js/2.944e1dcc.chunk.js"
+  },
+  {
+    "revision": "b28efdd56deb90065a6ed9c886c45c8b",
+    "url": "/static/js/2.944e1dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b2c690951f00892f51e460bac0e042",
+    "url": "/index.html"
+  }
+];
